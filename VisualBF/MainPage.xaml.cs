f0919f9a554f6bd83e40f1d9c68e4b839fe702d2
Curtis Rutland <insertAlias@gmail.com>
2011-05-04 15:59:21 -0500

    Included execution controls and operator buttons

--- VisualBF/MainPage.xaml.cs
+++ VisualBF/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Text;
 using System.Windows.Threading;
+using System.ComponentModel;
 
 namespace VisualBF {
     public partial class MainPage : UserControl {
@@ -22,6 +23,10 @@ namespace VisualBF {
         private List<byte> data;
         private List<BoxControl> dataBoxes, sourceBoxes;
         private Stack<int> loopIndexes;
+        private bool isRunning = false;
+        private List<Button> opButtons;
+
+        private double delay { get { return Math.Round(delaySlider.Value, 1); } }
 
         DispatcherTimer timer;
 
@@ -30,12 +35,13 @@ namespace VisualBF {
             dPtr = 0;
             iPtr = 0;
             data = Enumerable.Repeat<byte>(0, 100).ToList();
-            source = "";
+            //source = "";
             dataBoxes = new List<BoxControl>();
             sourceBoxes = new List<BoxControl>();
             loopIndexes = new Stack<int>();
             timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(.05) };
             timer.Tick += new EventHandler(timer_Tick);
+            opButtons = opStackPanel.Children.OfType<Button>().ToList();
             InitializeUI();
         }
 
@@ -53,60 +59,83 @@ namespace VisualBF {
             return bf;
         }
 
-        private void AddSourceHyperlinkButton_Click(object sender, RoutedEventArgs e) {
+        private BoxControl GetNewSourceBox(int i, string val) {
+            var bf = new BoxControl(i, val, false, false) { Height = 30, Width = 30, ShowIndex = false };
+            return bf;
+        }
+
+        private void AddSourceFromClipboard_Click(object sender, RoutedEventArgs e) {
+            if (Clipboard.ContainsText()) {
+                AddSourceText(Clipboard.GetText());
+            }
+        }
+
+        private void EnterSourceInPrompt_Click(object sender, RoutedEventArgs e) {
             Prompt p = new Prompt("Enter BF Source.");
             EventHandler f = null;
             f = (s, ea) => {
                 p.Closed -= f;
                 if (p.DialogResult ?? false)
-                    AddSource(p.Response);
+                    AddSourceText(p.Response);
             };
             p.Closed += f;
             p.Show();
         }
 
-        private void AddSource(string src) {
-            StringBuilder sb = new StringBuilder();
+        private void AddSourceText(string src) {
+            Queue<char> queue = new Queue<char>();
             foreach (char c in src)
                 if (BFOPS.Contains(c))
-                    sb.Append(c);
-            source = sb.ToString();
-            for (int i = 0; i < source.Length; i++) {
-                var bf = new BoxControl(i, source[i].ToString(), false, false) { Height = 30, Width = 30, ShowIndex = false };
+                    queue.Enqueue(c);
+            //source = sb.ToString();
+            int i = sourceBoxes.Count;
+            //for (; i < source.Length; i++) {
+            while (queue.Any()) {
+                var bf = GetNewSourceBox(i++, queue.Dequeue().ToString());
                 sourceWrapPanel.Children.Add(bf);
                 sourceBoxes.Add(bf);
             }
-            sourceBoxes.First().Data.IsActive = true;
-            executeHyperlinkButton.Visibility = System.Windows.Visibility.Visible;
+            source = sourceBoxes.Select(x => x.Data.Value).Aggregate((w, n) => w + n);
+            UpdateUI();
         }
 
-        private void ExecuteHyperlinkButton_Click(object sender, RoutedEventArgs e) {
-            dPtr = 0;
+        private void ClearSource_Click(object sender, RoutedEventArgs e) {
+            ClearSource();
+        }
+
+        private void ClearSource() {
+            timer.Stop();
+            isRunning = false;
+            source = "";
+            sourceBoxes.Clear();
+            sourceWrapPanel.Children.Clear();
+            Reset();
+        }
+
+        private void Reset() {
             iPtr = 0;
-            TryExecuteInstruction();
-            timer.Start();
-        }
-
-        private void ExecuteNonVisiblyHyperlinkButton_Click(object sender, RoutedEventArgs e) {
-            //dPtr = 0;
-            //iPtr = 0;
-            //TryExecuteInstruction();
-            //timer.Start();
+            dPtr = 0;
+            for (int i = 0; i < data.Count; i++)
+                data[i] = 0;
+            outputTextBlock.Text = "";
+            UpdateUI();
         }
 
         void timer_Tick(object sender, EventArgs e) {
             TryExecuteInstruction();
         }
 
-        private void TryExecuteInstruction() {
-            if (iPtr >= source.Length) {
+        private bool TryExecuteInstruction() {
+            if (iPtr >= sourceBoxes.Count) {
                 timer.Stop();
-                var bf = new BoxControl(iPtr, "END", false, false) { Height = 30, Width = 30, ShowIndex = false };
-                sourceWrapPanel.Children.Add(bf);
+                isRunning = false;
+                UpdateUI();
+                return false;
             }
-            else
-                ExecuteInstruction();
-            UpdateUI();
+            ExecuteInstruction();
+            if (delay > 0)
+                UpdateUI();
+            return true;
         }
 
         private void UpdateUI() {
@@ -116,19 +145,30 @@ namespace VisualBF {
                 dataBoxes[i].Data.Value = data[i].ToString();
                 dataBoxes[i].Data.IsActive = i == dPtr;
             }
-            for (int i = 0; i < source.Length; i++) {
+            for (int i = 0; i < sourceBoxes.Count; i++) {
                 sourceBoxes[i].Data.IsActive = i == iPtr;
             }
-            //UpdateLayout();
+            opButtons.ForEach(x => x.IsEnabled = iPtr >= sourceBoxes.Count);
+            sourceContextMenu.IsEnabled = !isRunning;
+            playButton.IsEnabled = !isRunning;
+            stepButton.IsEnabled = !isRunning;
+            delaySlider.IsEnabled = !isRunning;
+            inputTextBlock.IsReadOnly = isRunning;
+            pauseButton.IsEnabled = isRunning;
         }
 
         private void ExecuteInstruction() {
-            switch (source[iPtr]) {
+
+            switch (sourceBoxes[iPtr].Data.Value[0]) {
                 case '>':
                     ++dPtr;
                     break;
                 case '<':
                     --dPtr;
+                    if (dPtr < 0) {
+                        SetErrorCondition("Can't move pointer left of 0");
+                        return;
+                    }
                     break;
                 case '+':
                     ++data[dPtr];
@@ -140,15 +180,25 @@ namespace VisualBF {
                     outputTextBlock.Text += ((char)data[dPtr]).ToString();
                     break;
                 case ',':
-                    var input = RequestInput();
+                    data[dPtr] = GetInput();
                     break;
                 case '[':
                     loopIndexes.Push(iPtr);
                     int endLoopIndex = source.FindBalancingBrace(iPtr);
-                    if (data[dPtr] == 0)
-                        iPtr = endLoopIndex;
+                    if (data[dPtr] == 0) {
+                        if (endLoopIndex == -1) {
+                            SetErrorCondition("Syntax error: no matching close for start of loop.");
+                            return;
+                        }
+                        else
+                            iPtr = endLoopIndex;
+                    }
                     break;
                 case ']':
+                    if (!loopIndexes.Any()) {
+                        SetErrorCondition("Syntax error: no matching start for end of loop.");
+                        return;
+                    }
                     if (data[dPtr] != 0)
                         iPtr = loopIndexes.Peek();
                     else loopIndexes.Pop();
@@ -157,8 +207,62 @@ namespace VisualBF {
             ++iPtr;
         }
 
-        private string RequestInput() {
-            throw new NotImplementedException();
+        private void SetErrorCondition(string error) {
+            MessageBox.Show(error);
+            ClearSource();
+        }
+
+        private byte GetInput() {
+            if (inputTextBlock.Text == string.Empty)
+                return 0;
+            char c = inputTextBlock.Text[0];
+            if (c == (char)13)
+                c = (char)10;
+            inputTextBlock.Text = inputTextBlock.Text.Substring(1);
+            return (byte)c;
+        }
+
+        private void OpButton_Click(object sender, RoutedEventArgs e) {
+            string op = (sender as Button).Tag as string;
+            AddSourceText(op);
+            TryExecuteInstruction();
+        }
+
+        private void delaySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            if (delayTextBlock != null) {
+                delayTextBlock.Text = delay + " s";
+                timer.Interval = TimeSpan.FromSeconds(delay);
+            }
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e) {
+            isRunning = false;
+            timer.Stop();
+            UpdateUI();
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e) {
+            if (iPtr >= sourceBoxes.Count)
+                Reset();
+            isRunning = true;
+            if (delay > 0) {
+                timer.Start();
+                TryExecuteInstruction();
+            }
+            else {
+                while (TryExecuteInstruction()) { UpdateUI(); }
+            }
+        }
+
+        private void stepButton_Click(object sender, RoutedEventArgs e) {
+            if (iPtr >= sourceBoxes.Count)
+                Reset();
+            TryExecuteInstruction();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e) {
+            var about = new About();
+            about.Show();
         }
     }
 }
